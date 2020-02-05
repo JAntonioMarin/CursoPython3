563ac221db14bc6a9a2c2d6ad642960e0c9015e8
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-05 18:41:09 +0100

    Ejercicio 2

---
+++ Assignment/2.py
@@ -0,0 +1,8 @@
+consumo = 5.5
+distanciaTrabajo = 15
+precio = 1.02
+numeroDias = 20
+
+gasto = (((consumo / 100) * (distanciaTrabajo * 2)) * numeroDias) * precio
+
+print("Vas a gastar", gasto, "€")
